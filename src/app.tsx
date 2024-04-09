a10d102e8edc7837245b37acab66b2c6d8af07a9
fsabre <fabien.sabre@gmail.com>
2024-04-09 19:01:01 +0200

    feat: Add a base image, with custom pins over each existing symbol

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,34 @@
 import "./app.css";
 
+import baseImg from "./img/base.jpg";
+import radiantPin from "./img/radiant-pin.png";
+
+const STARTING_POINT: [number, number] = [132, 329];
+const X_GAP = 565;
+const Y_GAP = 82;
+const N_COLS = 4;
+const N_ROWS = 11;
+const pinCoords: [number, number][] = [];
+for (let x = 0; x < N_COLS; x++) {
+    for (let y = 0; y < N_ROWS; y++) {
+        pinCoords.push([STARTING_POINT[0] + x * X_GAP, STARTING_POINT[1] + y * Y_GAP]);
+    }
+}
+
+
 export function App() {
     return (
-        <div>
-        </div>
+        <>
+            {pinCoords.map((coords, i) =>
+                <img
+                    key={i}
+                    class="pin"
+                    src={radiantPin}
+                    alt={`pin${i}`}
+                    style={{left: coords[0], top: coords[1]}}
+                />
+            )}
+            <img src={baseImg} alt={"Base image"}/>
+        </>
     );
 }
