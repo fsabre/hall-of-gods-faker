7c55f083ef386de2306622a8e0a4c3ca1083fadb
fsabre <fabien.sabre@gmail.com>
2024-04-09 19:37:09 +0200

    feat: Allow to cycle symbols by clicking on them

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,18 @@
-import "./app.css";
+import {useState} from "preact/hooks";
 
+import "./app.css";
 import baseImg from "./img/base.jpg";
+import nonePin from "./img/none-pin.png";
+import attunedPin from "./img/attuned-pin.png";
+import ascendedPin from "./img/ascended-pin.png";
 import radiantPin from "./img/radiant-pin.png";
+
+enum PinType {
+    NONE,
+    ATT,
+    ASC,
+    RAD,
+}
 
 const STARTING_POINT: [number, number] = [132, 329];
 const X_GAP = 565;
@@ -15,18 +26,38 @@ for (let x = 0; x < N_COLS; x++) {
     }
 }
 
+interface PinProps {
+    pinNumber: number;
+    coords: [number, number];
+}
+
+function Pin(props: PinProps) {
+    const [symbol, setSymbol] = useState(PinType.RAD);
+
+    const {pinNumber, coords} = props;
+    const alias = `pin${pinNumber}`;
+
+    function rotate() {
+        setSymbol((symbol + 1) % 4);
+    }
+
+    return (
+        <img
+            key={alias}
+            className="pin"
+            src={symbol == PinType.RAD ? radiantPin : symbol == PinType.ASC ? ascendedPin : symbol == PinType.ATT ? attunedPin : nonePin}
+            alt={alias}
+            style={{left: coords[0], top: coords[1]}}
+            onClick={rotate}
+        />
+    );
+}
 
 export function App() {
     return (
         <>
             {pinCoords.map((coords, i) =>
-                <img
-                    key={i}
-                    class="pin"
-                    src={radiantPin}
-                    alt={`pin${i}`}
-                    style={{left: coords[0], top: coords[1]}}
-                />
+                <Pin pinNumber={i} coords={coords}/>
             )}
             <img src={baseImg} alt={"Base image"}/>
         </>
